266365e34183d0134541d269df821f4640297d59
unknown <rejin@gecidukki.ac.in>
2022-12-31 00:34:38 +0530

    'fir'

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
     res.render('index',{title:'Rejin'});
 });
+app.get('/:name',(req,res)=>{
+    res.render('index',{title:req.params.name});
+});
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
