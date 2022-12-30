212c90b353ad965ad34bcb0bf9e6e8c799caf3b5
unknown <rejin@gecidukki.ac.in>
2022-12-31 00:06:31 +0530

    'fir'

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const app=new express();
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.send('Welcome to CGPC APTI');
+    res.render('index',{title:'Rejin'});
 });
 
 app.listen(3000,()=>{
