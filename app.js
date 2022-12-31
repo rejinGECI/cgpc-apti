fabe5b137da70ff4f8acaf5a459d291f08c5d492
unknown <rejin@gecidukki.ac.in>
2022-12-31 23:33:01 +0530

    'cgpc-apti-start'

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const bodyParser=require('body-parser');
 const app=new express();
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render('index');
+});
+app.get("/signup",(req,res)=>{
+    res.render('signup');
 })
 app.post("/signup",(req,res)=>{
-    res.write('Post Works');
-    res.end();
+    console.log(req.body);
 })
 app.listen(3000,()=>{
     console.log("Server listening on port 3000");
---
+++ public/signup-script.js
@@ -0,0 +1,40 @@
+var xhr=new XMLHttpRequest();
+uemail=document.getElementById('uemail');
+umob=document.getElementById('umob');
+upswd=document.getElementById('upswd');
+urpswd=document.getElementById('urpswd');
+var submitbtn=document.getElementById('submit');
+submitbtn.disabled=true;
+// const params={
+//     email:uemail.value,
+//     mob:umob.value,
+//     pswd:upswd.value,
+//     rpswd:urpswd.value
+// }
+console.log(urpswd);
+urpswd.addEventListener('blur',()=>{
+    if(upswd.value!==urpswd.value){
+        hid.innerHTML='Password doesnt match';
+        hid.style.color='red';
+    }else{        
+        submitbtn.disabled=false;
+    }
+})
+
+
+
+/*
+xhr.open('POST','/signup',false);
+xhr.setRequestHeader('Content-type','application/json');
+var hid=document.getElementById('hid');
+
+xhr.onload=function(){
+    console.log(this.responseText);
+    hid.innerHTML=this.responseText;
+}
+
+submitbtn.addEventListener('click',function(e){
+    console.log("Clicked");
+    e.preventDefault();
+    xhr.send(JSON.stringify(params));
+})*/
