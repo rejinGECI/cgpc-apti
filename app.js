5f8f5ff9066143c1fce2e8ff30047565cacb0b44
unknown <rejin@gecidukki.ac.in>
2022-12-31 16:17:36 +0530

    'cgpc-apti-start'

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-const mongoose = require('mongoose');
-const Questions=require('./Questions');
-
-
-const url = `mongodb+srv://rejinGECI:12ej11v%40R@cluster0.xbu1qk3.mongodb.net/cgpc-apti?retryWrites=true&w=majority`;
-
-const connectionParams={
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-}
-mongoose.connect(url,connectionParams)
-    .then( () => {
-        title='DB Connected';
-        console.log('Connected to database ')
-    })
-    .catch( (err) => {
-        title='Failed';
-        console.error(`Error connecting to the database. \n${err}`);
-    })
-
-const question1=new Questions({
-    qnId:101,
-    qn:'Which is the capital of India',
-    options:['Tvm','New Delhi','Kollam','Kerala'],
-    author:'Rejin R'
-});
-
-async function f(){
-    await question1.save();
-}
-
-f();
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=new express();
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    // console.log(database);
-    res.render('index',{title:title});
-});
-app.get('/:name',(req,res)=>{
-    res.render('home',{nam:req.params.name});
-});
-
+app.get("/",(req,res)=>{
+    res.render('index');
+})
+app.post("/signup",(req,res)=>{
+    res.write('Post Works');
+    res.end();
+})
 app.listen(3000,()=>{
-    console.log('Server listening on port 3000');
+    console.log("Server listening on port 3000");
 })
